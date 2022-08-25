a57028aee11eda060149634eb0e8a09dd791dd0d
Marcelo Alves <selfmadecelo@gmail.com>
2022-08-24 19:37:35 -0700

    chore: update deps

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { theme } from './theme'
 
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/700.css'
+
 const container = document.getElementById('root')
 const root = createRoot(container as HTMLElement)
 
